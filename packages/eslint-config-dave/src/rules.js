ae804d528753998a4ab19af50c64f5dc09754227
Dave Caruso <dave@davecode.net>
2022-07-24 23:20:43 -0400

    fix: arrow functions with promises wont trigger eslint

--- packages/eslint-config-dave/src/rules.js
+++ packages/eslint-config-dave/src/rules.js
@@ -212,133 +212,145 @@ const rules = {
   '@typescript-eslint/adjacent-overload-signatures': 'error',
   '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
   '@typescript-eslint/await-thenable': 'error',
-  '@typescript-eslint/ban-ts-comment': ['warn', {
-    // I allow these but only with proper descriptions, since there are useful cases I
-    // use `@ts-ignore` in my code. If the usage is stupid and not required, it should
-    // just fail the code review.
-    minimumDescriptionLength: 1,
-    'ts-expect-error': 'allow-with-description',
-    'ts-ignore': 'allow-with-description',
-    'ts-nocheck': 'allow-with-description',
-  }],
+  '@typescript-eslint/ban-ts-comment': [
+    'warn',
+    {
+      // I allow these but only with proper descriptions, since there are useful cases I
+      // use `@ts-ignore` in my code. If the usage is stupid and not required, it should
+      // just fail the code review.
+      minimumDescriptionLength: 1,
+      'ts-expect-error': 'allow-with-description',
+      'ts-ignore': 'allow-with-description',
+      'ts-nocheck': 'allow-with-description',
+    },
+  ],
   // TODO: Add custom rules for this next one. Could consider having `{}` and `Object` tell user
   // about a nonexistant type alias in `@paperdave/types` for `NonNullish` or whatever.
   '@typescript-eslint/ban-types': 'error',
   '@typescript-eslint/class-literal-property-style': ['error', 'fields'],
-  "@typescript-eslint/consistent-generic-constructors": "error",
-  "@typescript-eslint/consistent-indexed-object-style": "error",
-  "@typescript-eslint/consistent-type-assertions": "error",
-  "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-  "@typescript-eslint/consistent-type-exports": ["error", { "fixMixedExportsWithInlineTypeSpecifier": true }],
-  "@typescript-eslint/consistent-type-imports": ["error", { disallowTypeAnnotations: false }],
-  "@typescript-eslint/default-param-last": "error",
-  "@typescript-eslint/dot-notation": ["error", {
-    allowIndexSignaturePropertyAccess: true,
-    allowPrivateClassPropertyAccess: true,
-    allowProtectedClassPropertyAccess: true
-  }],
-  "@typescript-eslint/explicit-function-return-type": "off", // I dislike this, as I practically am married to inferred types, though I understand how that is nice.
-  "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: 'no-public' }],
+  '@typescript-eslint/consistent-generic-constructors': 'error',
+  '@typescript-eslint/consistent-indexed-object-style': 'error',
+  '@typescript-eslint/consistent-type-assertions': 'error',
+  '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+  '@typescript-eslint/consistent-type-exports': [
+    'error',
+    { fixMixedExportsWithInlineTypeSpecifier: true },
+  ],
+  '@typescript-eslint/consistent-type-imports': ['error', { disallowTypeAnnotations: false }],
+  '@typescript-eslint/default-param-last': 'error',
+  '@typescript-eslint/dot-notation': [
+    'error',
+    {
+      allowIndexSignaturePropertyAccess: true,
+      allowPrivateClassPropertyAccess: true,
+      allowProtectedClassPropertyAccess: true,
+    },
+  ],
+  '@typescript-eslint/explicit-function-return-type': 'off', // I dislike this, as I practically am married to inferred types, though I understand how that is nice.
+  '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
   // With this one I foresee a lot of problems, but I think it's a good idea to have it on.
   // Setting to warn for now so it gets caught in code reviews.
-  "@typescript-eslint/explicit-module-boundary-types": ["warn", {
-    allowArgumentsExplicitlyTypedAsAny: true,
-  }],
-  "@typescript-eslint/member-delimiter-style": "off", // Handled by Prettier
+  '@typescript-eslint/explicit-module-boundary-types': [
+    'warn',
+    {
+      allowArgumentsExplicitlyTypedAsAny: true,
+    },
+  ],
+  '@typescript-eslint/member-delimiter-style': 'off', // Handled by Prettier
   // This does not have an auto-fix, which is really anoying. I refuse to enable this until
   // https://github.com/typescript-eslint/typescript-eslint/issues/2296 happens, but its been
   // two years with no PR, maybe i'll do it myself lol
   // "@typescript-eslint/member-ordering": ["error", {}],
-  "@typescript-eslint/method-signature-style": ["error", "method"],
-  "@typescript-eslint/naming-convention": "off", // Code is too diverse; You'll run into a package that doesn't do
-  "@typescript-eslint/no-array-constructor": "error",
-  "@typescript-eslint/no-base-to-string": "error",
-  "@typescript-eslint/no-confusing-non-null-assertion": "error",
-  "@typescript-eslint/no-confusing-void-expression": ["error", { ignoreArrowShorthand: true }],
-  "@typescript-eslint/no-dupe-class-members": "error",
-  "@typescript-eslint/no-duplicate-enum-values": "error",
-  "@typescript-eslint/no-dynamic-delete": "error",
-  "@typescript-eslint/no-empty-function": "off", // Eslint will count a filled function as ANYTHING, even a blank comment. For that reason, it's not worth having this rule.
-  "@typescript-eslint/no-empty-interface": "error", // This is the stupidest rule ever invented.
-  "@typescript-eslint/no-explicit-any": "off", // This is a good rule, I just love my any. I'll promise to try and document what and why I use it
-  "@typescript-eslint/no-extra-non-null-assertion": "error", // I was not aware this was even valid code.
-  "@typescript-eslint/no-extra-parens": "off", // Prettier handles this
-  "@typescript-eslint/no-extra-semi": "off", // Prettier handles this
-  "@typescript-eslint/no-extraneous-class": "off",
-  "@typescript-eslint/no-floating-promises": "off", // This will lead to a bunch of .catch(() => {}), which is worse than just throwing them IMO.
-  "@typescript-eslint/no-for-in-array": "error",
-  "@typescript-eslint/no-implied-eval": "off",
-  "@typescript-eslint/no-inferrable-types": "error",
-  "@typescript-eslint/no-invalid-this": "off", // I thought the TypeScript compiler would catch this.
-  "@typescript-eslint/no-invalid-void-type": "error",
-  "@typescript-eslint/no-loop-func": "error",
-  "@typescript-eslint/no-loss-of-precision": "error",
-  "@typescript-eslint/no-magic-numbers": "off", // TODO: get the sanity to handle using this
-  "@typescript-eslint/no-meaningless-void-operator": "error",
-  "@typescript-eslint/no-misused-new": "error",
-  "@typescript-eslint/no-misused-promises": "error",
-  "@typescript-eslint/no-namespace": "error", // Namespaces are evil. Use ESM instead.
-  "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
-  "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
+  '@typescript-eslint/method-signature-style': ['error', 'method'],
+  '@typescript-eslint/naming-convention': 'off', // Code is too diverse; You'll run into a package that doesn't do
+  '@typescript-eslint/no-array-constructor': 'error',
+  '@typescript-eslint/no-base-to-string': 'error',
+  '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+  '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
+  '@typescript-eslint/no-dupe-class-members': 'error',
+  '@typescript-eslint/no-duplicate-enum-values': 'error',
+  '@typescript-eslint/no-dynamic-delete': 'error',
+  '@typescript-eslint/no-empty-function': 'off', // Eslint will count a filled function as ANYTHING, even a blank comment. For that reason, it's not worth having this rule.
+  '@typescript-eslint/no-empty-interface': 'error', // This is the stupidest rule ever invented.
+  '@typescript-eslint/no-explicit-any': 'off', // This is a good rule, I just love my any. I'll promise to try and document what and why I use it
+  '@typescript-eslint/no-extra-non-null-assertion': 'error', // I was not aware this was even valid code.
+  '@typescript-eslint/no-extra-parens': 'off', // Prettier handles this
+  '@typescript-eslint/no-extra-semi': 'off', // Prettier handles this
+  '@typescript-eslint/no-extraneous-class': 'off',
+  '@typescript-eslint/no-floating-promises': 'off', // This will lead to a bunch of .catch(() => {}), which is worse than just throwing them IMO.
+  '@typescript-eslint/no-for-in-array': 'error',
+  '@typescript-eslint/no-implied-eval': 'off',
+  '@typescript-eslint/no-inferrable-types': 'error',
+  '@typescript-eslint/no-invalid-this': 'off', // I thought the TypeScript compiler would catch this.
+  '@typescript-eslint/no-invalid-void-type': 'error',
+  '@typescript-eslint/no-loop-func': 'error',
+  '@typescript-eslint/no-loss-of-precision': 'error',
+  '@typescript-eslint/no-magic-numbers': 'off', // TODO: get the sanity to handle using this
+  '@typescript-eslint/no-meaningless-void-operator': 'error',
+  '@typescript-eslint/no-misused-new': 'error',
+  '@typescript-eslint/no-misused-promises': 'error',
+  '@typescript-eslint/no-namespace': 'error', // Namespaces are evil. Use ESM instead.
+  '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
+  '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
   // Same with no-any and ban-ts-comment, you'll just add eslint ignores everywhere instead of documenting why. But
   // also alot of these cases you can just infer based on usage that it's ok to use the assert.
-  "@typescript-eslint/no-non-null-assertion": "off",
-  "@typescript-eslint/no-redeclare": "error",
-  "@typescript-eslint/no-redundant-type-constituents": "error",
-  "@typescript-eslint/no-require-imports": "off",
-  "@typescript-eslint/no-restricted-imports": "off", // We do not have any restricted imports.
-  "@typescript-eslint/no-shadow": "error",
-  "@typescript-eslint/no-this-alias": "off", // I never had to use a `const this`, but I still feel like it's fine to allow.
-  "@typescript-eslint/no-throw-literal": "error", // This should be enforced by the JS runtime tbh.
-  "@typescript-eslint/no-type-alias": "off", // No.
-  "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-  "@typescript-eslint/no-unnecessary-condition": "off",
-  "@typescript-eslint/no-unnecessary-qualifier": "error",
-  "@typescript-eslint/no-unnecessary-type-arguments": "error",
-  "@typescript-eslint/no-unnecessary-type-assertion": "error",
-  "@typescript-eslint/no-unnecessary-type-constraint": "error",
-  "@typescript-eslint/no-unsafe-argument": "off", // any is supposed to just work, not be limited.
-  "@typescript-eslint/no-unsafe-assignment": "off", // any is supposed to just work, not be limited.
-  "@typescript-eslint/no-unsafe-call": "off", // any is supposed to just work, not be limited.
-  "@typescript-eslint/no-unsafe-member-access": "off", // any is supposed to just work, not be limited.
-  "@typescript-eslint/no-unsafe-return": "off", // any is supposed to just work, not be limited.
+  '@typescript-eslint/no-non-null-assertion': 'off',
+  '@typescript-eslint/no-redeclare': 'error',
+  '@typescript-eslint/no-redundant-type-constituents': 'error',
+  '@typescript-eslint/no-require-imports': 'off',
+  '@typescript-eslint/no-restricted-imports': 'off', // We do not have any restricted imports.
+  '@typescript-eslint/no-shadow': 'error',
+  '@typescript-eslint/no-this-alias': 'off', // I never had to use a `const this`, but I still feel like it's fine to allow.
+  '@typescript-eslint/no-throw-literal': 'error', // This should be enforced by the JS runtime tbh.
+  '@typescript-eslint/no-type-alias': 'off', // No.
+  '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+  '@typescript-eslint/no-unnecessary-condition': 'off',
+  '@typescript-eslint/no-unnecessary-qualifier': 'error',
+  '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+  '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+  '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+  '@typescript-eslint/no-unsafe-argument': 'off', // any is supposed to just work, not be limited.
+  '@typescript-eslint/no-unsafe-assignment': 'off', // any is supposed to just work, not be limited.
+  '@typescript-eslint/no-unsafe-call': 'off', // any is supposed to just work, not be limited.
+  '@typescript-eslint/no-unsafe-member-access': 'off', // any is supposed to just work, not be limited.
+  '@typescript-eslint/no-unsafe-return': 'off', // any is supposed to just work, not be limited.
   '@typescript-eslint/no-unused-expressions': 'off', // This is turned off since every line of code will be red underlined as you type.
-  "@typescript-eslint/no-unused-vars": "warn",
-  "@typescript-eslint/no-use-before-define": "off", // TypeScript checker catches this
-  "@typescript-eslint/no-useless-constructor": "off",
-  "@typescript-eslint/no-useless-empty-export": "error",
-  "@typescript-eslint/no-var-requires": "off",
-  "@typescript-eslint/non-nullable-type-assertion-style": "error",
-  "@typescript-eslint/parameter-properties": "off", // While confusing, these properties are really useful.
-  "@typescript-eslint/prefer-as-const": "error",
-  "@typescript-eslint/prefer-enum-initializers": "off", // I don't think we need this.
-  "@typescript-eslint/prefer-for-of": "error",
-  "@typescript-eslint/prefer-function-type": "error",
-  "@typescript-eslint/prefer-includes": "error",
-  "@typescript-eslint/prefer-literal-enum-member": "error",
-  "@typescript-eslint/prefer-namespace-keyword": "off", // We do not use namespaces.
-  "@typescript-eslint/prefer-nullish-coalescing": "error",
-  "@typescript-eslint/prefer-optional-chain": "off", // TypeScript compiler handles this.
-  "@typescript-eslint/prefer-readonly": "off", // This will flag all things until you finish writing your code.
-  "@typescript-eslint/prefer-readonly-parameter-types": "off", // Kind of ugly ngl
-  "@typescript-eslint/prefer-reduce-type-parameter": "error",
-  "@typescript-eslint/prefer-regexp-exec": "error",
-  "@typescript-eslint/prefer-return-this-type": "error",
-  "@typescript-eslint/prefer-string-starts-ends-with": "error",
-  "@typescript-eslint/prefer-ts-expect-error": "error",
-  "@typescript-eslint/promise-function-async": "error",
-  "@typescript-eslint/require-array-sort-compare": ["error", { ignoreStringArrays: true }],
-  "@typescript-eslint/require-await": "warn",
-  "@typescript-eslint/restrict-plus-operands": "off", // Doesn't work.
-  "@typescript-eslint/restrict-template-expressions": "off",
-  "@typescript-eslint/return-await": ["error", "always"], // Forcing await will add an extra line to the stack trace, to improve debugging of where the promise was actually created.
-  "@typescript-eslint/sort-type-union-intersection-members": "off", // Sometimes we want a custom order of a union.
-  "@typescript-eslint/strict-boolean-expressions": "off",
-  "@typescript-eslint/switch-exhaustiveness-check": "error",
-  "@typescript-eslint/triple-slash-reference": "off", // These have uses still
-  "@typescript-eslint/typedef": "error",
-  "@typescript-eslint/unbound-method": "off",
-  "@typescript-eslint/unified-signatures": 'off', // Does not work right. see @paperdave/utils' range() function.
+  '@typescript-eslint/no-unused-vars': 'warn',
+  '@typescript-eslint/no-use-before-define': 'off', // TypeScript checker catches this
+  '@typescript-eslint/no-useless-constructor': 'off',
+  '@typescript-eslint/no-useless-empty-export': 'error',
+  '@typescript-eslint/no-var-requires': 'off',
+  '@typescript-eslint/non-nullable-type-assertion-style': 'error',
+  '@typescript-eslint/parameter-properties': 'off', // While confusing, these properties are really useful.
+  '@typescript-eslint/prefer-as-const': 'error',
+  '@typescript-eslint/prefer-enum-initializers': 'off', // I don't think we need this.
+  '@typescript-eslint/prefer-for-of': 'error',
+  '@typescript-eslint/prefer-function-type': 'error',
+  '@typescript-eslint/prefer-includes': 'error',
+  '@typescript-eslint/prefer-literal-enum-member': 'error',
+  '@typescript-eslint/prefer-namespace-keyword': 'off', // We do not use namespaces.
+  '@typescript-eslint/prefer-nullish-coalescing': 'error',
+  '@typescript-eslint/prefer-optional-chain': 'off', // TypeScript compiler handles this.
+  '@typescript-eslint/prefer-readonly': 'off', // This will flag all things until you finish writing your code.
+  '@typescript-eslint/prefer-readonly-parameter-types': 'off', // Kind of ugly ngl
+  '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+  '@typescript-eslint/prefer-regexp-exec': 'error',
+  '@typescript-eslint/prefer-return-this-type': 'error',
+  '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+  '@typescript-eslint/prefer-ts-expect-error': 'error',
+  '@typescript-eslint/promise-function-async': ['error', { checkArrowFunctions: false }],
+  '@typescript-eslint/require-array-sort-compare': ['error', { ignoreStringArrays: true }],
+  '@typescript-eslint/require-await': 'warn',
+  '@typescript-eslint/restrict-plus-operands': 'off', // Doesn't work.
+  '@typescript-eslint/restrict-template-expressions': 'off',
+  '@typescript-eslint/return-await': ['error', 'always'], // Forcing await will add an extra line to the stack trace, to improve debugging of where the promise was actually created.
+  '@typescript-eslint/sort-type-union-intersection-members': 'off', // Sometimes we want a custom order of a union.
+  '@typescript-eslint/strict-boolean-expressions': 'off',
+  '@typescript-eslint/switch-exhaustiveness-check': 'error',
+  '@typescript-eslint/triple-slash-reference': 'off', // These have uses still
+  '@typescript-eslint/typedef': 'error',
+  '@typescript-eslint/unbound-method': 'off',
+  '@typescript-eslint/unified-signatures': 'off', // Does not work right. see @paperdave/utils' range() function.
   // "@typescript-eslint/unified-signatures": ["error", { ignoreDifferentlyNamedParameters: true }],
 };
 
