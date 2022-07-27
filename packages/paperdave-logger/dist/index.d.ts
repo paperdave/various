89e6f78d1f9dc88dfedfc1c74932480d9e3ff5cc
Dave Caruso <dave@davecode.net>
2022-07-27 01:07:21 -0400

    fix: dont mind me lmfao

--- packages/paperdave-logger/dist/index.d.ts
+++ packages/paperdave-logger/dist/index.d.ts
@@ -1,1 +1,11 @@
-export * from '../src';
+import * as log from './log';
+export { colors } from './ansi';
+export { CLIError, type PrintableError } from './error';
+export { injectLogger } from './inject';
+export { LogLevel, setLevel } from './level';
+export * from './log';
+export { BarStyle, Progress, withProgress, type BarSpinnerOptions, type ProgressOptions, type WithProgressOptions, } from './progress';
+export { Spinner, withSpinner, type SpinnerOptions } from './spinner';
+export { isUnicodeSupported, logSymbols } from './unicode';
+export { LogWidget } from './widget';
+export default log;
